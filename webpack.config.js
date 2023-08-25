c647f5197621f6aa31bb4d9f20a79abf027cc7de
Will Klancnik <william@sisudata.com>
2023-08-25 22:58:23 +0000

    Add simple app

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  devtool: "source-map", // Source map generation must be turned on
+  plugins: [
+    sentryWebpackPlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+
+      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+      // and need `project:releases` and `org:read` scopes
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      telemetry: false,
+      debug: true
+    }),
+  ],
+};
